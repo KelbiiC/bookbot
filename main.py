f548ea0a1db4c1aa9404b39c4c9d8ec0bc5089e3
KelbiiC <changk0430@gmail.com>
2025-01-20 07:15:45 -0600

    implemented word count function

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
-    print(file_contents)
+    print(total_word_count(file_contents))
+
+def total_word_count(text):
+    return len(text.split())
 
 main()
